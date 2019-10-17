76827ba048f1b94bcc3ba36751922f1dfc5b26f4
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-10-17 23:48:30 +0100

    Use ID on each recipe

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -1,19 +1,27 @@
 <template>
+<nuxt-link :to="'/recipes/' + id">
     <article class="recipe">
         <div class="thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}">
         </div>
         <h1>{{ title }}</h1>
         <p>{{ previewText }}</p>
     </article>
+</nuxt-link>
+    
 </template>
 
 <script>
 export default {
-    props: ['thumbnail', 'title', 'previewText']
+    props: ['thumbnail', 'title', 'previewText', 'id']
 }
 </script>
 
 <style scoped>
+
+a {
+    text-decoration: none;
+    color: black;
+}
 
 .recipe {
     box-sizing: border-box;
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,13 +1,15 @@
 <template>
     <section class="recipes">
         <Recipe
-        thumbnail="https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg"
-        title="Delicious Hamburger"
-        previewText="Awesome Hamburger!"
+            thumbnail="https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg"
+            title="Delicious Hamburger"
+            id="1"
+            previewText="Awesome Hamburger!"
         />
         <Recipe 
             thumbnail="https://www.medicalnewstoday.com/content/images/articles/324/324956/close-up-of-a-plate-of-food.jpg"
             title="Delicious vegetables"
+            id="2"
             previewText="Awesome vegetables"
         />
     </section>
@@ -18,8 +20,11 @@ import Recipe from '@/components/Recipe'
 export default {
     components: {
         Recipe
+    },
+    asyncData() {
+
     }
-}
+};
 </script>
 
 <style scoped>
