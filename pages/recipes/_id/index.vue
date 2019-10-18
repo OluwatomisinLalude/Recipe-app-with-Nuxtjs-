ca2fd7cf3e065033c4269e22343f32ddf22d12eb
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2019-10-18 01:11:22 +0100

    Using Promises. Create Header component

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -2,13 +2,43 @@
     <section>
     <h1>{{ $route.params.id }}</h1>
         <div>
-            <img src="" alt="">
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
         </div>
-        <p>The recipe description</p>
+        <p>{{ recipe.descriptionText }}</p>
     </section>
 </template>
 
+<script>
+export default {
+    asyncData(context) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    recipe: [
+                        {
+                            id: "1",
+                            title: "Delicious hamburger",
+                            previewText: "Awesome hamburger",
+                            descriptionText: "What a tasty snack!",
+                            thumbnail: "https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg"
+                        },
+                        {
+                            id:"2",
+                            title: "Delicious vegetables",
+                            previewText: "Awesome vegetables",
+                            descriptionText: "What a tasty meal!",
+                            thumbnail: "https://www.medicalnewstoday.com/content/images/articles/324/324956/close-up-of-a-plate-of-food.jpg"
+                        }
+                    ].find(el => el.id === context.params.id)
+                })
+            }, 1500)
+        })
+    }
+};
+</script>
+
 <style scoped>
+
 .single-recipe {
     display: flex;
     flex-flow: column;
@@ -16,5 +46,9 @@
     align-items: center;
     text-align: center;
     padding: 30px;
+}
+
+.recipe-image {
+    width: 40%;
 }
 </style>
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,28 +1,44 @@
 <template>
     <section class="recipes">
         <Recipe
-            thumbnail="https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg"
-            title="Delicious Hamburger"
-            id="1"
-            previewText="Awesome Hamburger!"
-        />
-        <Recipe 
-            thumbnail="https://www.medicalnewstoday.com/content/images/articles/324/324956/close-up-of-a-plate-of-food.jpg"
-            title="Delicious vegetables"
-            id="2"
-            previewText="Awesome vegetables"
+            v-for="recipe in recipes"
+            :key="recipe.id"
+            :thumbnail="recipe.thumbnail"
+            :title="recipe.title"
+            :id="recipe.id"
+            :previewText="recipe.previewText"
         />
     </section>
 </template>
 
 <script>
 import Recipe from '@/components/Recipe'
+import { resolve, reject } from 'q'
 export default {
     components: {
         Recipe
     },
     asyncData() {
-
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({
+                    recipes: [
+                        {
+                            id: "1",
+                            title: "Delicious hamburger",
+                            previewText: "Awesome hamburger",
+                            thumbnail: "https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg"
+                        },
+                        {
+                            id:"2",
+                            title: "Delicious vegetables",
+                            previewText: "Awesome vegetables",
+                            thumbnail: "https://www.medicalnewstoday.com/content/images/articles/324/324956/close-up-of-a-plate-of-food.jpg"
+                        }
+                    ]
+                })
+            }, 1500)
+        })
     }
 };
 </script>
